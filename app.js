22c7cc42ecee8234dde1de40446aaf008972253a
tomas <tbrusa@itba.edu.ar>
2021-04-07 16:14:48 -0300

    mensaje sin abrir comando

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 console.log('hola');
+//git init    a la carpeta que se usa
+//git status  para ver quienes lo estan usando
+//git add     para agregar al archivo
+//git config para mail y nombre
+//git commit
+//escribo algo apreto esc y :wq
+//git log  para ver los que he creado
+//git checkout -- archivo     me lo saca a la version guardada
+// git diff archivo    me dice lo que cambio en la version
+// git add archivo     para guardar los nuevos cambios
+// y luego apreto git commit
